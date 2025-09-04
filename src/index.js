27f81b6c92366dd124dfa53f2c92fee3a358959e
KM Rejoan Tanjim <kmrejoantanjim@gmail.com>
2025-09-05 02:11:27 +0600

    created apiError , apiResponse , asyncHandler function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./db/db.js";
+import { app } from "./app.js";
+const port = process.env.PORT || 3000;
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`server is running at port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection failed!!!", err);
+  });
 
-connectDB();
 /*
 import express from "express";
 const app = express()(async () => {
